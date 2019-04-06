5d94698338d2759cdd9155adee41a167655141a7
Daria <mamontova.dasha@gmail.com>
2019-04-06 13:26:29 +0600

    Добавлены сериализаторы для заказа и логина.

--- source/api_v1/serializers.py
+++ source/api_v1/serializers.py
@@ -39,6 +39,46 @@ class InlineProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ('id', 'name')
 
+class OrderSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:order-detail')
+    order_products = InlineProductSerializer(many=True, read_only=True)
 
+    class Meta:
+        model = Order
+        fields = ('url', 'id', 'products', 'phone', 'address', 'comment', 'created_at', 'order_products')
+
+class UserSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:user-detail')
+    username = serializers.CharField(read_only=True)
+    password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    new_password_confirm = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    email = serializers.EmailField(required=True, allow_blank=False)
+
+    def validate_password(self, value):
+        user = self.context['request'].user
+        if not authenticate(username=user.username, password=value):
+            raise ValidationError('Invalid password for your account')
+        return value
+
+    def validate(self, attrs):
+        if attrs.get('new_password') != attrs.get('new_password_confirm'):
+            raise ValidationError("Passwords do not match")
+        return super().validate(attrs)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'first_name', 'last_name', 'email',
+                  'password', 'new_password', 'new_password_confirm']
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    token = serializers.CharField(write_only=True)
+
+    def validate_token(self, token):
+        try:
+            return Token.objects.get(key=token)
+        except Token.DoesNotExist:
+            raise ValidationError("Invalid credentials")
 
 
