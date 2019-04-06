9ef013f7d576900010bb510649a771fb1fe97b49
Daria <mamontova.dasha@gmail.com>
2019-04-06 12:12:55 +0600

    Создана модель Категории товара, мигрирована.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+
+
