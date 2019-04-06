b143756d4f4c16efbad9b7b574987e5f9539e7c0
Daria <mamontova.dasha@gmail.com>
2019-04-06 12:29:55 +0600

    Модели зарегистрированы в админке.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from webapp.models import Category, Product, Photo, Order
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    ordering = ['name']
+    search_fields = ['name', 'id']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    ordering = ['name']
+    search_fields = ['pk', 'name']
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'product']
+    search_fields = ['pk', 'product']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user']
+    search_fields = ['pk', 'user']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Order, OrderAdmin)
