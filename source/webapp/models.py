5e66bd3c840531eb633f6f1eb8e11a09e71bfdd9
Daria <mamontova.dasha@gmail.com>
2019-04-06 12:13:49 +0600

    Создана модель Товара, мигрирована.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -12,6 +12,18 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    date = models.DateField()
+    categories = models.ManyToManyField(Category, related_name='categories', blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Products'
 
 
 
