762a2c60a105093e4179f607b9709fd23980aff0
Daria <mamontova.dasha@gmail.com>
2019-04-06 13:34:03 +0600

    Добавлены компоненты: пункт меню, логотип, меню, навбар.

---
+++ frontend/src/components/Navbar/Menu/Menu.js
@@ -0,0 +1,53 @@
+import React, {Component, Fragment} from 'react'
+import MenuItem from "./MenuItem/MenuItem";
+import {connect} from "react-redux";
+import Layout from "../../Layout/Layout";
+
+
+class Menu extends Component {
+    state = {
+        collapse: true
+    };
+
+    toggle = () => {
+        this.setState({collapse: !this.state.collapse});
+    };
+
+    render() {
+        // const {username, is_admin, user_id} = this.props.auth;
+        return <Fragment>
+            <button onClick={this.toggle}
+                    className="navbar-toggler"
+                    type="button"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"/>
+            </button>
+            <div className={(this.state.collapse ? "collapse" : "") + " navbar-collapse"}
+                 id="navbarNav">
+                <ul className="navbar-nav">
+                    {/*<MenuItem to="/">Фильмы</MenuItem>*/}
+                    {/*{is_admin ? <MenuItem to="/movies/add">Добавить фильм</MenuItem> : null}*/}
+                    {/*<MenuItem to="/halls/">Залы</MenuItem>*/}
+                    {/*{is_admin ? <MenuItem to="/halls/add">Добавить зал</MenuItem> : null}*/}
+                </ul>
+                <ul className="navbar-nav ml-auto">
+                    {/*{user_id ? [*/}
+                        {/*<MenuItem to={"/users/" + user_id} key="username"><span className="navbar-text">Привет, {username}!</span></MenuItem>,*/}
+                        {/*<MenuItem to="/logout" key="logout">Выйти</MenuItem>*/}
+                    {/*] : [*/}
+                        {/*<MenuItem to="/login" key="login">Войти</MenuItem>,*/}
+                        {/*<MenuItem to="/register" key="register">Зарегистрироваться</MenuItem>*/}
+                    {/*]}*/}
+                </ul>
+            </div>
+        </Fragment>
+    }
+}
+
+
+const mapStateToProps = state => ({auth: state.auth});
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
